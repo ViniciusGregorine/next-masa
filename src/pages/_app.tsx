2457617f84b36c4b87d2249072e332304872aac0
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-10-20 08:02:23 -0300

    feat: add `IconContext.Provider`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
+import { IconContext } from 'react-icons/lib'
 
 import '../styles/global.scss'
+import '../styles/reactIcon.scss'
 
 function MyApp({ Component, pageProps }: AppProps) { 
   return (
-        <Component {...pageProps} />
+    <IconContext.Provider value={{ className: 'react-icons' }}>
+      <Component {...pageProps} />
+    </IconContext.Provider>
+
   )
 }
 
