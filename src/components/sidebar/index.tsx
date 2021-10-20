2b048ff67b4143d5577789d008396df091ae6af7
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-10-20 08:00:58 -0300

    feat: add sidebar icons

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,17 +1,21 @@
+import {MdSensors} from 'react-icons/md'
+import {BsFillFileBarGraphFill} from 'react-icons/bs'
+import { FaBox } from 'react-icons/fa'
+
 import styles from './styles.module.scss'
 
 const Sidebar = () => (
   <aside className={styles.sidebarContainer}>
     <nav className={styles.nav}>
         <ul>
-          <li>
-            readings
+          <li className="nav_list">
+            <BsFillFileBarGraphFill/>
           </li>
-          <li>
-            sensors
+          <li  className="nav_list">
+            <MdSensors/>
           </li>
-          <li>
-            environment
+          <li  className="nav_list">
+            <FaBox/>
           </li>
         </ul>
     </nav>
