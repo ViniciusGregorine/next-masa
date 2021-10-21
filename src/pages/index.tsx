19ee88e26861cb081ca67582aed63f11693d6409
ViniciusGregorine <viniciusgmenegon@gmail.com>
2021-10-21 10:23:51 -0300

    feat: listing places on `index.tsx`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,46 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout";
+import List from "../components/List";
+import { SimplePlace } from "../interfaces/place";
+import { getPlace } from "../services/api";
 
-const IndexPage = () => (
-  <Layout>
-    <h1 className="bg-red text-opacity-0 ">Hello aa Next.js 👋</h1>
-    <p>
-      <p>teste</p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+
+
+
+const IndexPage = () => {
+  const [places, setPlaces] = useState<any>([{}])
+
+  async function get () {
+    const axiosPlaces: any = await getPlace()
+
+    const simplePlace: SimplePlace = axiosPlaces.map((place: any) => ({
+      id: place.id,
+      description: place.description,
+      note: place.note
+    }))
+  
+    setPlaces(simplePlace)
+  }
+useEffect( () => {
+  get()
+}, [])
+
+  return (
+    <Layout>
+      <h1>Ambientes</h1>
+      <hr/>
+     
+
+        <List items={places}></List>
+        
+        <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+}
 
 export default IndexPage;
